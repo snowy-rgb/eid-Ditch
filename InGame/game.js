7a809d327290b523e8e616c43f4c77df3846bb78
snowy <trigger.snowii@gmail.com>
2025-03-02 13:43:47 +0900

    Update game.js

--- InGame/game.js
+++ InGame/game.js
@@ -19,6 +19,14 @@ const player = {
     onGround: false
 };
 
+// 카메라 설정
+const camera = {
+    x: 0,
+    y: 0,
+    width: canvas.width,
+    height: canvas.height
+};
+
 // 키 입력 상태 저장
 const keys = {
     left: false,
@@ -66,6 +74,13 @@ function movePlayer() {
     }
 }
 
+// 카메라 업데이트 함수 (플레이어를 따라가기)
+function updateCamera() {
+    // 카메라가 플레이어를 따라가도록 설정 (부드러운 이동)
+    camera.x += (player.x - camera.x - canvas.width / 2) * 0.1;
+    camera.y += (player.y - camera.y - canvas.height / 2) * 0.1;
+}
+
 // 게임 루프
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -73,14 +88,24 @@ function gameLoop() {
     // 플레이어 이동 적용
     movePlayer();
 
+    // 카메라 업데이트
+    updateCamera();
+
+    // 캔버스를 기준으로 좌표 이동
+    ctx.save();
+    ctx.translate(-camera.x, -camera.y); // 카메라 위치 보정
+
     // 플레이어 그리기
     ctx.fillStyle = "white";
     ctx.fillRect(player.x, player.y, player.width, player.height);
+
+    ctx.restore(); // 원래 상태로 복귀
 
     requestAnimationFrame(gameLoop);
 }
 
 // 게임 시작
 gameLoop();
+
 
 
